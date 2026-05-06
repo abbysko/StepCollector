474deba1c105af8cd0338fc2d5ce321d80a7ba0b
Abby Skofield <abigailskofield@gmail.com>
2026-05-06 14:48:34 -0400

    Initial commit (most workflows available, but steps not correctly tracked.)

---
+++ SimpleStepTracker/SimpleStepTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SimpleStepTracker
+//
+//  Created by Abigail Skofield on 4/13/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SimpleStepTracker/SimpleStepTracker/SimpleStepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleStepTrackerApp.swift
+//  SimpleStepTracker
+//
+//  Created by Abigail Skofield on 4/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleStepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
